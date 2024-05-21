81adb753b0e6397119fb4609cbf91a6671b68be0
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-05-21 17:29:55 +0000

    Products view, urls, updated project level url new directory products and products template also added

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+def shop_all(request):
+    """ Show all products together with sorting order and search selection """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+    
+    return render (request, 'products/products.html', context)
