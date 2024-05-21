7e3fed836832fd526d97794f8af12615d6722fc4
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-05-21 22:28:31 +0000

    Search functionality has been added and fully working with any specific word in the name of the product or description

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,  reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -6,9 +8,22 @@ def shop_all(request):
     """ Show all products together with sorting order and search selection """
 
     products = Product.objects.all()
+    query = None
+
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did not enter any search criteria")
+                return redirect(reverse ('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
-        'products': products
+        'products': products,
+        'search_term': query,
     }
     
     return render (request, 'products/products.html', context)
