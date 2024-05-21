c64f8232c8ee6704d4b0f316f1d20830e2232a06
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-05-21 20:13:40 +0000

    product detail page added along with the view and url, related files updated for the correct functionality and view

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('', views.shop_all, name='products')
+    path('', views.shop_all, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
 ]
