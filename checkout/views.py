72c175347d276dbecd4daf23d93a3c3a9af1bdd4
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-06-05 12:28:17 +0000

    Added stripe elements, added style for the checkout page in css file and newly created js file to match

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,8 @@ def  checkout(request):
     template = 'checkout/checkout.html' 
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OL4m2LdZm4Sj6hUXqkQ3Vx5V82CGQanfQFYBCoXFTnszOCcuZTpBjle75SfL8kaIrgRvAT60OaghddXvhtLyY5c001fT4z5h7',
+        'client_secret': 'test client secret',
     }
     return render(request, template, context)
 
