1cf398dcc3aa4460a7abff353f6343d7ad5d93dc
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-06-14 14:22:10 +0000

    Add product functionality is completed and working successfully through product management nav item in the mobile top header and in the main nav on the home page it's all links messages image field also updated to work along

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from .import views
 
 urlpatterns = [
     path('', views.shop_all, name='products'),
-    path('int:product_id/', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
 ]
