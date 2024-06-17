ae5ae0139de09e2c4fc24a03a7c7ef168afaf0cb
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-06-17 10:44:16 +0000

    Removed secret key and set debug

--- jolie_mimi_boutique/settings.py
+++ jolie_mimi_boutique/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-716(d4a463kca%ehxbwfj_#80!f8zeqewb6^boo6kg#kggo=cq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['jolie-mimi-boutique-ad3e13f83c61.herokuapp.com', 'localhost']
 
