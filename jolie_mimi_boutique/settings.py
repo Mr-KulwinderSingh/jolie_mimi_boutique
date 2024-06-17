6c064697230be2b8e4d9ce70f321366cd2a6b350
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2024-06-17 19:19:09 +0000

    settings updated with the correct name of bucket

--- jolie_mimi_boutique/settings.py
+++ jolie_mimi_boutique/settings.py
@@ -182,7 +182,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Bucket config
 if "USE_AWS" in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'jolie_mimi_boutique'
+    AWS_STORAGE_BUCKET_NAME = 'jolie-mimi-boutique'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
