c650573ae1d7fd905f2088f75609ae61afe1b46d
Kulwinder Singh <k29singh@gmail.com>
2025-10-22 16:42:31 +0100

    Switch from Gmail SMTP to SendGrid via Anymail

--- jolie_mimi_boutique/settings.py
+++ jolie_mimi_boutique/settings.py
@@ -68,6 +68,7 @@ INSTALLED_APPS = [
     #Other
     'crispy_forms',
     'storages',
+    'anymail',
 ]
 
 MIDDLEWARE = [
@@ -240,15 +241,32 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+# if 'DEVELOPMENT' in os.environ:
+#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#     DEFAULT_FROM_EMAIL = 'joliemimiboutique@example.com'
+# else:
+#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#     # EMAIL_USE_TLS = True
+#     # EMAIL_PORT = 587
+#     # EMAIL_HOST = 'smtp.gmail.com'
+#     # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+#     # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+#     # DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    
+
+# Email settings
+
 if 'DEVELOPMENT' in os.environ:
+    # Local development: print emails to console
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'joliemimiboutique@example.com'
+
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-    
+    # Production (Heroku) - SendGrid setup
+    EMAIL_BACKEND = "anymail.backends.sendgrid.EmailBackend"
+    ANYMAIL = {
+        "SENDGRID_API_KEY": os.environ.get("SENDGRID_API_KEY"),
+    }
+
+    DEFAULT_FROM_EMAIL = "noreply@yourdomain.com"  # Replace with your verified sender or domain
+    SERVER_EMAIL = DEFAULT_FROM_EMAIL
