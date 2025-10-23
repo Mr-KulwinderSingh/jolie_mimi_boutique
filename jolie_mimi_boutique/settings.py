ccbb082ae437d00a1c3d05053cbaa3f9f664e687
Kulwinder Singh <k29singh@gmail.com>
2025-10-23 17:47:45 +0100

    Update confirmation email & subject

--- jolie_mimi_boutique/settings.py
+++ jolie_mimi_boutique/settings.py
@@ -1,5 +1,3 @@
-
-
 import os
 
 SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
@@ -243,19 +241,6 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-
-# if 'DEVELOPMENT' in os.environ:
-#     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-#     DEFAULT_FROM_EMAIL = 'joliemimiboutique@example.com'
-# else:
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#     # EMAIL_USE_TLS = True
-#     # EMAIL_PORT = 587
-#     # EMAIL_HOST = 'smtp.gmail.com'
-#     # EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-#     # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-#     # DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-    
 
 # Email settings
 
